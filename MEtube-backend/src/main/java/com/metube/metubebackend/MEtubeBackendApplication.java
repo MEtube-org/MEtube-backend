ca7b270ddc020a2ff599d4e68083593d1bb50b67
AlexePaul <alexepaul2011@yahoo.com>
2024-04-05 14:52:45 +0300

    Added a default spring project

---
+++ MEtube-backend/src/main/java/com/metube/metubebackend/MEtubeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.metube.metubebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MEtubeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MEtubeBackendApplication.class, args);
+    }
+
+}
---
+++ MEtube-backend/src/test/java/com/metube/metubebackend/MEtubeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metube.metubebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MEtubeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
